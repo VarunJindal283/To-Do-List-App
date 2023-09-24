c9beaedf11a30381661b0301b5055742c6978398
Varun <varun2394.be21@chitkara.edu.in>
2023-09-25 00:07:29 +0530

    add the functionality of toggle and remove

--- script.js
+++ script.js
@@ -13,4 +13,14 @@ function addTask(){
         span.innerHTML="\u00d7";
         li.appendChild(span);
     }
+    inputBox.value="";
 }
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
